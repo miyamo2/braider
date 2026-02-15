e80eee17134f12bbde7e834ae727065a8799ea0b
miyamo2 <miyamo2@outlook.com>
2026-02-16 00:25:50 +0900

    feat(app): introduce app options and defined-container support

--- internal/annotation/app.go
+++ internal/annotation/app.go
@@ -5,3 +5,21 @@ type AppMarker struct{}
 type App interface {
 	_IsApp() AppMarker
 }
+
+type AppOptionMarker struct{}
+
+type AppOption interface {
+	_IsAppOption() AppOptionMarker
+}
+
+type AppDefaultMarker struct{}
+
+type AppDefault interface {
+	_IsAppDefault() AppDefaultMarker
+}
+
+type AppContainerMarker struct{}
+
+type AppContainer interface {
+	_IsAppContainerMarker() AppContainerMarker
+}
